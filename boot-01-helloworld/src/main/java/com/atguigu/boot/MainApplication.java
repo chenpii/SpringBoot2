ec74565f91eb3c8600e8daca14635207aa02ae84
chenh <641760512@qq.com>
2022-10-17 21:54:39 +0800

    boot-01-helloworld

---
+++ boot-01-helloworld/src/main/java/com/atguigu/boot/MainApplication.java
@@ -0,0 +1,19 @@
+package com.atguigu.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主程序类
+ *
+ * @author chenpi
+ * @SpringBootApplication 这是一个Spring boot应用
+ * @create 2022-10-17 20:23
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
---
+++ boot-01-helloworld/src/main/java/com/atguigu/boot/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.atguigu.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chenpi
+ * @create 2022-10-17 20:25
+ */
+// @Controller
+// @ResponseBody
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String handler01() {
+        return "Hello Spring Boot2!";
+    }
+
+
+}
