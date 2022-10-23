c0c14ad05ad60469fbaed82565ac0b124ad35c3d
chenh <641760512@qq.com>
2022-10-23 22:52:02 +0800

    表单REST请求（_method）

--- boot-05-web-01/src/main/java/com/atguigu/boot/controller/HelloController.java
+++ boot-05-web-01/src/main/java/com/atguigu/boot/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.atguigu.boot.controller;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -14,7 +15,26 @@ public class HelloController {
     @RequestMapping("/bug.jpg")
     public String hello() {
         return "hello";
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.GET)
+    public String getUser(){
+        return "GET-张三";
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.POST)
+    public String saveUser(){
+        return "POST-张三";
+    }
 
 
+    @RequestMapping(value = "/user",method = RequestMethod.PUT)
+    public String putUser(){
+        return "PUT-张三";
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.DELETE)
+    public String deleteUser(){
+        return "DELETE-张三";
     }
 }
