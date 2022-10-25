255e540b13c6137346877b9e72d76df82213555a
chenp <641760512@qq.com>
2022-10-25 17:42:56 +0800

    矩阵变量请求

--- boot-05-web-01/src/main/java/com/atguigu/boot/config/Webconfig.java
+++ boot-05-web-01/src/main/java/com/atguigu/boot/config/Webconfig.java
@@ -3,14 +3,25 @@ package com.atguigu.boot.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.UrlPathHelper;
 
 @Configuration(proxyBeanMethods = false)
-public class Webconfig {
+public class Webconfig implements WebMvcConfigurer {
 
     @Bean
     public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
         HiddenHttpMethodFilter methodFilter = new HiddenHttpMethodFilter();
         methodFilter.setMethodParam("_m");
         return methodFilter;
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        UrlPathHelper urlPathHelper = new UrlPathHelper();
+        //不移除分号后面的内容，使矩阵变量功能生效
+        urlPathHelper.setRemoveSemicolonContent(false);
+        configurer.setUrlPathHelper(urlPathHelper);
     }
 }
--- boot-05-web-01/src/main/java/com/atguigu/boot/controller/RequsetController.java
+++ boot-05-web-01/src/main/java/com/atguigu/boot/controller/RequsetController.java
@@ -1,13 +1,12 @@
 package com.atguigu.boot.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -31,6 +30,22 @@ public class RequsetController {
         map.put("anno_code", code);
         map.put("anno_msg", msg);
         map.put("attribute_msg1", msg1);
+        return map;
+    }
+
+    // 矩阵变量请求
+    // 1、语法 /cars/sell;low=34;brand=byd,audi,yd
+    // 2、SpringBoot默认禁用了矩阵变量的功能。
+    //              手动开启：原理。对于路径的处理。UrlPathHelper进行解析。
+    //              removeSemicolonContent（移除分号内容）支持矩阵变量的
+    @GetMapping("/cars/{path}")
+    public Map carsell(@MatrixVariable("low") Integer low,
+                       @MatrixVariable("brand") List<String> brand,
+                       @PathVariable("path") String path) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("low", low);
+        map.put("brand", brand);
+        map.put("path", path);
         return map;
     }
 
