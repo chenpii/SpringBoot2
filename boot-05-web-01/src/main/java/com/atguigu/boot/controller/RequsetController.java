3fe1bae37cb2a8fb958b1bacacc523ba6e061ea7
chenh <641760512@qq.com>
2022-11-01 23:01:59 +0800

    测试SpringMVC 对于特殊请求参数的解析

--- boot-05-web-01/src/main/java/com/atguigu/boot/controller/RequsetController.java
+++ boot-05-web-01/src/main/java/com/atguigu/boot/controller/RequsetController.java
@@ -1,9 +1,12 @@
 package com.atguigu.boot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,20 +21,45 @@ public class RequsetController {
         return "forward:/success"; //转发到 /success
     }
 
+    @GetMapping("/params")
+    public String testParam(Map<String, Object> map,
+                            Model model,
+                            HttpServletRequest request,
+                            HttpServletResponse response) {
+
+        map.put("hello", "world666");
+        model.addAttribute("world", "hello666");
+        request.setAttribute("message", "helloworld");
+        Cookie cookie = new Cookie("c1", "v1");
+        cookie.setDomain("localhost");
+        response.addCookie(cookie);
+        return "forward:/success";
+
+    }
+
     @ResponseBody
     @GetMapping("/success")
-    public Map success(@RequestAttribute("msg") String msg,
-                       @RequestAttribute("code") Integer code,
+    public Map success(@RequestAttribute(value = "msg", required = false) String msg,
+                       @RequestAttribute(value = "code", required = false) Integer code,
                        HttpServletRequest request) {
 
         Map<String, Object> map = new HashMap<>();
         String msg1 = (String) request.getAttribute("msg");
+
+        Object hello = request.getAttribute("hello");
+        Object world = request.getAttribute("world");
+        Object message = request.getAttribute("message");
+
         map.put("anno_code", code);
         map.put("anno_msg", msg);
         map.put("attribute_msg1", msg1);
+
+        map.put("hello", hello);
+        map.put("world", world);
+        map.put("message", message);
+
         return map;
     }
-
 
 
 }
