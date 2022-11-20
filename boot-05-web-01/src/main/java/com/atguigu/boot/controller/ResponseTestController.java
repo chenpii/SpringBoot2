fdc7cc33790299168e1f0d6e9e83677f1fc7b651
chenh <641760512@qq.com>
2022-11-20 21:23:17 +0800

    响应内容协商

--- boot-05-web-01/src/main/java/com/atguigu/boot/controller/ResponseTestController.java
+++ boot-05-web-01/src/main/java/com/atguigu/boot/controller/ResponseTestController.java
@@ -1,6 +1,7 @@
 package com.atguigu.boot.controller;
 
 import com.atguigu.boot.bean.Person;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,7 +15,15 @@ import java.util.Date;
 @Controller
 public class ResponseTestController {
 
-    @ResponseBody
+    @ResponseBody //--RequestResponseBodyMethodProcessor ---> messageConvert
+    @GetMapping("/he111")
+    public FileSystemResource file() {
+
+        //文件以这样的方式返回，看是谁处理的
+        return null;
+    }
+
+    @ResponseBody //利用返回值处理器里面的消息转换器进行处理,将返回值写成json格式
     @GetMapping("/test/person")
     public Person getPerson() {
         Person person = new Person();
