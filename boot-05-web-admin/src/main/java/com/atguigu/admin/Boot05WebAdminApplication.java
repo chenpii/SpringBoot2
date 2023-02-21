0564d6b7d4feb9281ffe081df4767666203b166e
chenp <641760512@qq.com>
2023-02-21 20:32:57 +0800

    模板引擎实验-后台管理系统

---
+++ boot-05-web-admin/src/main/java/com/atguigu/admin/Boot05WebAdminApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot05WebAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot05WebAdminApplication.class, args);
+    }
+
+}
---
+++ boot-05-web-admin/src/test/java/com/atguigu/admin/Boot05WebAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot05WebAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
