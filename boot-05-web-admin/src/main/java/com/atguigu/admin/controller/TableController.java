cf7504c4329b4a47681b76d88a4a93bc1702f0c9
chenp <641760512@qq.com>
2023-02-22 17:41:02 +0800

    table页面

--- boot-05-web-admin/src/main/java/com/atguigu/admin/controller/TableController.java
+++ boot-05-web-admin/src/main/java/com/atguigu/admin/controller/TableController.java
@@ -12,22 +12,22 @@ public class TableController {
     @GetMapping("/basic_table")
     public String basic_table() {
 
-        return "/table/basic_table";
+        return "table/basic_table";
     }
 
     @GetMapping("/dynamic_table")
     public String dynamic_table() {
 
-        return "/table/dynamic_table";
+        return "table/dynamic_table";
     }
 
     @GetMapping("/responsive_table")
     public String responsive_table() {
-        return "/table/responsive_table";
+        return "table/responsive_table";
     }
 
     @GetMapping("/editable_table")
     public String editable_table() {
-        return "/table/editable_table";
+        return "table/editable_table";
     }
 }
