0cc0123f5f4a0065ce26a29e064e5b4cac8f40de
admin <admin@example.com>
2023-06-29 10:41:43 +0800

    【P50】文件上传 MultipartFile 自动封装上传的文件

--- boot-05-web-admin/src/main/java/com/atguigu/admin/controller/FormTestController.java
+++ boot-05-web-admin/src/main/java/com/atguigu/admin/controller/FormTestController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * 文件上传测试
  */
@@ -33,9 +36,26 @@ public class FormTestController {
     public String upload(@RequestParam("email") String email,
                          @RequestParam("username") String username,
                          @RequestPart("headerImg") MultipartFile headerImg,
-                         @RequestPart("photos") MultipartFile[] photos) {
+                         @RequestPart("photos") MultipartFile[] photos) throws IOException {
 
         log.info("上传的信息：email={},username={},headerImg={},photos={}", email, username, headerImg.getSize(), photos.length);
+
+        if (!headerImg.isEmpty()) {
+            //获取上传的原始文件名
+            String originalFilename = headerImg.getOriginalFilename();
+            //保存到文件服务器
+            headerImg.transferTo(new File("I:\\cache\\" + originalFilename));
+        }
+
+        if (photos.length > 0) {
+            for (MultipartFile photo : photos) {
+                if (!photo.isEmpty()) {
+                    String originalFilename = photo.getOriginalFilename();
+                    photo.transferTo(new File("I:\\cache\\" + originalFilename));
+                }
+            }
+        }
+
         return "main";
     }
 }
