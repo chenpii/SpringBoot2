7498da4728c1e10635ec8652b9485513720f4ed5
admin <admin@example.com>
2023-07-04 14:50:19 +0800

    【P60】数据访问-数据库场景的自动配置分析与整合测试 连接MySql，使用jdbcTemplate完成查询

--- boot-05-web-admin/src/test/java/com/atguigu/admin/Boot05WebAdminApplicationTests.java
+++ boot-05-web-admin/src/test/java/com/atguigu/admin/Boot05WebAdminApplicationTests.java
@@ -1,13 +1,25 @@
 package com.atguigu.admin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+@Slf4j
 @SpringBootTest
 class Boot05WebAdminApplicationTests {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Test
     void contextLoads() {
+        // jdbcTemplate.queryForObject("select conut(1) from account_tb1");
+        // jdbcTemplate.queryForList("select conut(1) from account_tb1");
+        Long aLong = jdbcTemplate.queryForObject("select count(1) from account_tb1", Long.class);
+        log.info("记录总数: {}" , String.valueOf(aLong));
     }
+
 
 }
