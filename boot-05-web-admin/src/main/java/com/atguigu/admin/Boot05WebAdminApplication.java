97c77fbd17d2367974c4bac501e8f17d1af89024
admin <admin@example.com>
2023-07-05 18:35:28 +0800

    【P64】数据访问-整合mybatis-可以用Mapper扫描

--- boot-05-web-admin/src/main/java/com/atguigu/admin/Boot05WebAdminApplication.java
+++ boot-05-web-admin/src/main/java/com/atguigu/admin/Boot05WebAdminApplication.java
@@ -1,9 +1,11 @@
 package com.atguigu.admin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+// @MapperScan(basePackages = "com.atguigu.admin.mapper") //可以使用扫描器自动扫描mapper，但是还是推荐手动写@Mapper
 @ServletComponentScan(basePackages = "com.atguigu.admin")
 @SpringBootApplication
 public class Boot05WebAdminApplication {
