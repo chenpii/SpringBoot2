9c58b90c95075a93334a1580653414b6b0341162
admin <admin@example.com>
2023-07-10 16:47:50 +0800

    【P67】数据访问-crud实验-删除用户完成

--- boot-05-web-admin/src/main/java/com/atguigu/admin/controller/TableController.java
+++ boot-05-web-admin/src/main/java/com/atguigu/admin/controller/TableController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,6 +26,15 @@ public class TableController {
     public String basic_table() {
         // int i = 10 / 0; //制造异常
         return "table/basic_table";
+    }
+
+    @GetMapping("/user/delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id,
+                             @RequestParam(value = "pn", defaultValue = "1") Integer pn,
+                             RedirectAttributes ra) {
+        userService.removeById(id);
+        ra.addAttribute("pn", pn);
+        return "redirect:/dynamic_table";
     }
 
     @GetMapping("/dynamic_table")
